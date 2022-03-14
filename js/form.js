b330e459f6003a3de941bfe8c886d314aedf1633
Konstantin Surikov <surikov@gmail.com>
2022-03-14 23:01:13 +0300

    Added a validation for the title, price, rooms and capacity inputs

--- js/form.js
+++ js/form.js
@@ -1,43 +1,53 @@
-const DISABLE_CLASS = 'ad-form--disabled';
-const INTERACTIVE_ELEMENTS = [
-  'button', 'input', 'select', 'textarea'
-];
+const adForm = document.querySelector('.ad-form');
 
-const forms = [
-  document.querySelector('.map__filters'),
-  document.querySelector('.ad-form')
-];
-
-const toggleInputs = (enable) => {
-  forms.forEach((form) => {
-    INTERACTIVE_ELEMENTS.forEach((interactive) => {
-      form.querySelectorAll(interactive)
-        .forEach((element) => {element.disabled = !enable;});
-    });
-  });
+const capacityErrorMessages = {
+  '1': '1 комната для 1 гостя',
+  '2': '2 комнаты для 2 гостей или для 1 гостя',
+  '3': '3 комнаты для 3 гостей или для 2 гостей или для 1 гостя',
+  '100': '100 комнат не для гостей',
 };
 
-const toggleForms = (enable) => {
-  forms.forEach((form) => {
-    if (enable) {
-      form.classList.remove(DISABLE_CLASS);
-      return;
+const capacityOption = {
+  '1': ['1'],
+  '2': ['1', '2'],
+  '3': ['1', '2', '3'],
+  '100': ['0'],
+};
+
+const roomsField = adForm.querySelector('#room_number');
+const capacityField = adForm.querySelector('#capacity');
+
+const pristine = new Pristine(adForm, {
+  classTo: 'ad-form__element',
+  errorClass: 'ad-form__item--invalid',
+  successClass: 'ad-form__item--valid',
+  errorTextParent: 'ad-form__element',
+  errorTextTag: 'div',
+  errorTextClass: 'ad-form__error'
+});
+
+const getCapacityErrorMessage = () => capacityErrorMessages[roomsField.value];
+
+const validateCapacity = () => capacityOption[roomsField.value]
+  .includes(capacityField.value);
+
+pristine.addValidator(roomsField, validateCapacity, getCapacityErrorMessage);
+pristine.addValidator(capacityField, validateCapacity);
+
+const validateAddOfferForm = () => {
+  adForm.addEventListener('submit', (evt) => {
+    const isValid = pristine.validate();
+    if (!isValid) {
+      evt.preventDefault();
     }
-    form.classList.add(DISABLE_CLASS);
   });
-};
 
-const disablePage = () => {
-  toggleInputs(false);
-  toggleForms(false);
-};
-
-const enablePage = () => {
-  toggleInputs(true);
-  toggleForms(true);
+  roomsField.addEventListener('change', () => pristine.validate());
+  capacityField.addEventListener('change', () => pristine.validate());
 };
 
 export {
-  disablePage,
-  enablePage,
+  validateAddOfferForm,
 };
+
+
